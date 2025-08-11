f687f12a9e53ba1b53ef642f7952fbd2f5d203f8
minseung <119600579+mingeung@users.noreply.github.com>
2025-08-11 17:03:23 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 178 ms, Memory: 35,588 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -12,49 +12,59 @@ public class Solution {
 
 		Scanner sc = new Scanner(System.in);
 
-		// 0이 될 때 종료가 됨
+		// 제일 앞에 있는 숫자를 빼서, 가장 뒤에 넣는 것이니 선입선출(FIFO) 방식의 queue를 쓰는 게 좋다.
+
+		// 총 10개의 테스트케이스가 주어진다.
 		for (int T = 1; T <= 10; T++) {
 
-		// 큐 생성
-		Queue<Integer> q = new LinkedList<>();
+			// 큐 생성
+			Queue<Integer> q = new LinkedList<>();
 
-		int tc = sc.nextInt();
+			// 테스트케이스 번호 입력받기
+			int tc = sc.nextInt();
 
-		// 데이터를 담을 변수
+			// 큐에 각 암호 입력받기
+			for (int i = 0; i < 8; i++) {
+				q.offer(sc.nextInt());
+			}
 
-		for (int i = 0; i < 8; i++) {
-			q.offer(sc.nextInt());
-		}
+			// 감소시킬 숫자 1로 초기화
+			int count = 1;
 
-		int count = 1;
+			// while로 무한루프 돌리기
+			while (true) {
+				// 큐의 가장 앞에 있는 숫자를 꺼내서
+				int target = q.poll();
+				// count만큼 감소시키기
+				target -= count;
 
+				// 만약 감소시킨 수가 0보다 같거나 작아지는 경우는 0으로 유지되며 프로그램은 종료된다.
+				if (target <= 0) {
+					target = 0;
+					q.offer(target);
+					break;
+				}
 
-		// queue에서 맨 앞에 것을 poll해서 1을 감소하고 뒤로 보낸다
-		while (true) {
-
-			int target = q.poll() - count;
-			// target이 0이하이면 0으로 저장되고 정지됨
-			if (target <= 0) {
-				target = 0;
+				// 종료되는 경우가 아니라면 큐의 가장 뒤에 target값을 추가한다.
 				q.offer(target);
-				break;
+
+				// 감소되는 숫자는 1씩 작아져야 하므로 +1을 해준다.
+				count += 1;
+
+				// count가 5를 넘어가면 한 cycle을 돈 것이기 때문에 count 값을 1로 초기화시킨다.
+				if (count == 6) {
+					count = 1;
+				}
 			}
 
-			q.offer(target);
-
-			count += 1;
-			// count가 5를 넘어가면 초기화시킨다.
-			if (count == 6) {
-				count = 1;
+			// 출력로직
+			System.out.print("#" + tc + " ");
+			// for문으로 queue에서 poll로 값을 빼내면서 출력한다.
+			while (!q.isEmpty()) {
+				System.out.print(q.poll() + " ");
 			}
-		}
-		
-		System.out.print("#" + tc + " ");
-		for (int i = 0; i < 8; i++) {
-			System.out.print(q.poll() + " ");
-		}
 
-		System.out.println();
+			System.out.println();
 		}
 	}
 
